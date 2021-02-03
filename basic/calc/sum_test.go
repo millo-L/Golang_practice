eb153e198a5f20c0f0ba6e2d71870001539fefdd
seung <seungmin3837@naver.com>
2021-02-03 22:33:44 +0900

    basic

---
+++ basic/calc/sum_test.go
@@ -0,0 +1,37 @@
+package calc
+
+import "testing"
+
+type TestData struct {
+	argument1 int
+	argument2 int
+	result    int
+}
+
+var testdata = []TestData{
+	{2, 6, 8},
+	{-8, 3, -5},
+	{6, -6, 0},
+	{0, 0, 0},
+}
+
+func TestSum(t *testing.T) {
+	for _, d := range testdata {
+		r := Sum(d.argument1, d.argument2)
+		if r != d.result {
+			t.Errorf(
+				"The result of %d + %d is not %d r=%d",
+				d.argument1,
+				d.argument2,
+				d.result,
+				r,
+			)
+		}
+	}
+}
+
+func BenchmarkSum(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Sum(1, 2)
+	}
+}
